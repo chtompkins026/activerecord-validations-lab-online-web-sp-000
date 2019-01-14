098295e68754df0cab67502fcd1ccfaa54360ccd
Christopher Tompkins <chtompkins026@gmail.com>
2019-01-14 20:01:58 +0000

    Automatically backed up by Learn

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,2 +1,6 @@
 class Author < ActiveRecord::Base
+  validates :name 
+  validates(:bio, { :length => { :maximum => 500 } })
+  validates(:password, { :length => { :in => 6..20 } })
+  validates(:registration_number, { :length => { :is => 6 } })
 end
